ad3d92a2474991e33b0cfd9cb52b3109b0f77783
EKaczmarek <e.kaczmarek01@gmail.com>
2016-12-01 23:49:50 +0100

    Practice makes perfect

---
+++ 5/1.py
@@ -0,0 +1,10 @@
+def obj():
+	a = int(input('Długość podstawy: '))
+	b = int(input('Szerokość podstawy: '))
+	c = int(input('Wysokość: '))
+	wynik = a * b * c
+	print('Objetosc wynosi: %2.1f' %wynik)
+	
+
+obj()
+input()
